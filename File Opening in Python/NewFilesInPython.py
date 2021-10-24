5cd7651b14a99981327af47fcd6035538906b8e6
PranavVerma-droid <73458565+PranavVerma-droid@users.noreply.github.com>
2021-10-24 22:12:49 +0530

    Update NewFilesInPython.py

--- File Opening in Python/NewFilesInPython.py
+++ File Opening in Python/NewFilesInPython.py
@@ -2,5 +2,5 @@ import os
 import sys
 
 file = open("C:/Users/Pranav Verma/Desktop/Github/My-Python-Repo/File Opening in Python/NewFileCreatedInPython.txt", 'x')
-file.open("NewFileCreated") #After You run this file, this will create a newfile named above with the name given above. give it a try xD
+file.write("FileContentsGoHere") #After You run this file, this will create a newfile named above with the name given above. give it a try xD
 file.close()
