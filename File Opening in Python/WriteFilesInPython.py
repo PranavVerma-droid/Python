b3555d675f5791d02285dd333a6d72f4e2ae7078
PranavVerma-droid <73458565+PranavVerma-droid@users.noreply.github.com>
2021-10-24 22:07:14 +0530

    Create WriteFilesInPython.py

---
+++ File Opening in Python/WriteFilesInPython.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+file = open("C:/Users/Pranav Verma/Desktop/Github/My-Python-Repo/File Opening in Python/Demo.txt", 'w')
+file.write("CHICKEN NUGGET ALL AROUND XD")/n
+file.write("If you are wondering what happened to the file, DONT WORRY XD")
+file.close()
+
