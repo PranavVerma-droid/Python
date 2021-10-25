76c28518a27da9b1a7f937ffe9a5d64f3068aabd
PranavVerma-droid <73458565+PranavVerma-droid@users.noreply.github.com>
2021-10-25 09:14:05 +0530

    Create FileDeletionSystemInPython.py

---
+++ File Opening in Python/FileDeletionSystemInPython.py
@@ -0,0 +1,8 @@
+import os #Will Import OS from collections
+import sys #Will Import Sys from collections
+
+"""Creating a File First(Select this part and run it first)"""
+
+file = ("C:/Users/Pranav Verma/Desktop/FileDeletionSystem.txt", 'x')
+file.write("FileContents Go here")
+file.close()
