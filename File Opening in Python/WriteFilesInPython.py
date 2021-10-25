23e4d59b2667a998526ffbbec94eca249e2c9634
PranavVerma-droid <73458565+PranavVerma-droid@users.noreply.github.com>
2021-10-25 08:59:05 +0530

    Update WriteFilesInPython.py

--- File Opening in Python/WriteFilesInPython.py
+++ File Opening in Python/WriteFilesInPython.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 file = open("C:/Users/Pranav Verma/Desktop/Github/My-Python-Repo/File Opening in Python/Demo.txt", 'w')
-file.write("CHICKEN NUGGET ALL AROUND XD")/n
-file.write("If you are wondering what happened to the file, DONT WORRY XD")
+file.write("CHICKEN NUGGET ALL AROUND XD")
+#After you run this file, this will erase all the content in the DEMO.TXT File and replace it with the text above xD
 file.close()
 
