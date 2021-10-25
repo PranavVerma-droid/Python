94984ced4387aaaa1e6f7e975fdff69f463d5dd0
PranavVerma-droid <73458565+PranavVerma-droid@users.noreply.github.com>
2021-10-25 14:42:37 +0530

    Update Practice File.py

--- Basic Files/Practice File.py
+++ Basic Files/Practice File.py
@@ -4,7 +4,7 @@ print("Starting Console. Pls Wait...")
 mylist = [10, 20, 30, 'Pranav', 30]
 print(mylist)
 mylist.insert(3, 'MyString')
-mylist.append('Append Addes to the LAST of the list')
+mylist.append('Append Addes to the LAST of the list'
 print(mylist)
 
 mydictonary = {'reptiles': 'snake',
