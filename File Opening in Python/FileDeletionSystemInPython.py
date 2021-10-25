9426e84912e3c90a70495b387c72f83d3a2a9202
PranavVerma-droid <73458565+PranavVerma-droid@users.noreply.github.com>
2021-10-25 09:19:36 +0530

    Update FileDeletionSystemInPython.py

--- File Opening in Python/FileDeletionSystemInPython.py
+++ File Opening in Python/FileDeletionSystemInPython.py
@@ -1,8 +1,15 @@
-import os #Will Import OS from collections
-import sys #Will Import Sys from collections
-
 """Creating a File First(Select this part and run it first)"""
+import os
 
-file = ("C:/Users/Pranav Verma/Desktop/FileDeletionSystem.txt", 'x')
-file.write("FileContents Go here")
+file = open("C:/Users/Pranav Verma/Desktop/FileDeletionSystem.txt", 'x')
+file.write("This is a file to demonstrate the file deletion system in python")
 file.close()
+
+"""Deleting a File"""
+import os
+
+if os.path.exists("C:/Users/Pranav Verma/Desktop/FileDeletionSystem.txt"):
+    os.remove("C:/Users/Pranav Verma/Desktop/FileDeletionSystem.txt")
+else:
+    print("The File does not exist!")
+
