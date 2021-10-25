2078ba751e8f5e2378953776c28da73ac6f46fce
PranavVerma-droid <73458565+PranavVerma-droid@users.noreply.github.com>
2021-10-25 08:56:21 +0530

    Create FileHandlingSystemInPython.py

---
+++ File Opening in Python/FileHandlingSystemInPython.py
@@ -0,0 +1,6 @@
+import os
+import sys
+
+file = open("C:/Users/Pranav Verma/Desktop/Github/My-Python-Repo/File Opening in Python/NewFileCreatedInPython.txt", 'x')
+file.write("FileContentsGoHere") #After You run this file, this will create a newfile named above with the name given above. give it a try xD
+file.close()
