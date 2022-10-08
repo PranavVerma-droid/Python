0e8f60224a51b38065d4417f7d12fdfbed984162
Pranav Verma <pranav@verma.net.in>
2022-10-08 14:26:28 +0530

    Update Python Sets.py

--- Basic Files/Python Sets.py
+++ Basic Files/Python Sets.py
@@ -1,4 +1,4 @@
 #Python Sets are Unordered, No duplicate enteries are present.
 
 Set = {10,20,30,20,30,30,30,'Pranav'}
-print(Set)#Duplicate Values will be IGNORED in a set.
+print(Set) #Duplicate Values will be IGNORED in a set.
