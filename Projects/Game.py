3d1dd71c65f7691be276d178ac18d5730626c1b3
Pranav Verma <pranav@verma.net.in>
2023-05-11 22:05:11 +0530

    Update (idk lol)

---
+++ Projects/Game.py
@@ -0,0 +1,16 @@
+print("")
+print("Welcome to PyXZeph.")
+print("This is a text-sytled Game made in Python.")
+
+print("Choose your Character:")
+print("1. Warrior")
+print("2. Mage")
+print("3. Archer")
+print("4. Assassin")
+print("5. Knight")
+print("6. Paladin")
+print("7. Necromancer")
+print("8. Priest")
+
+character = input("Choose your Character: ")
+
