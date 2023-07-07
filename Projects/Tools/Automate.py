20f636754c441845423f5f1d02895cffaff08a60
Pranav Verma <pranav@verma.net.in>
2023-07-08 01:00:09 +0530

    Update

---
+++ Projects/Tools/Automate.py
@@ -0,0 +1,45 @@
+import pyautogui as pg
+import time
+
+# Giving Dealy to run program
+print("program will run after 10 second")
+time.sleep(10)
+print("running")
+
+# Note : after running the program immediately open whatsapp web then open the persons chat you want to send messages
+
+# For loop
+#for i in range(100):
+    # writing the messages
+    #pg.write("Message Bomb Hehe Boi")
+    #time.sleep(0.5)
+    # Seding the messages by pressing enter
+    #pg.press("Enter")
+
+pg.write("<html>")
+pg.press("Enter")
+pg.write("<head>")
+pg.press("Enter")
+pg.write("<title>")
+pg.press("Enter")
+pg.write("This is a test")
+pg.press("Enter")
+pg.write("</title>")
+pg.press("Enter")
+pg.write("</head>")
+pg.press("Enter")
+pg.write("<body>")
+pg.press("Enter")
+pg.write("<h1>")
+pg.write("<center>")
+pg.press("Enter")
+pg.write("Hello World")
+pg.press("Enter")
+pg.write("</h1>")
+pg.write("</center>")
+pg.press("Enter")
+pg.write("</body>")
+pg.press("Enter")
+pg.press("Backspace")
+pg.write("</html>")
+pg.press("Enter")
