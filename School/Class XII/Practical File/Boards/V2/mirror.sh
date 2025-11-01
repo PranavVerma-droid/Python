a3e2bfff03466a40c1e1ae3d05694312acbebbf0
Pranav Verma <pranav@verma.net.in>
2025-11-01 21:28:20 +0530

    Update: Added Chapter 4.

--- School/Class XII/Practical File/Boards/V2/mirror.sh
+++ School/Class XII/Practical File/Boards/V2/mirror.sh
@@ -1,6 +1,54 @@
 #!/bin/bash
 # ====== PASTE ANYTHING BELOW THIS LINE ======
 : <<'CONTENT'
+1. Add name
+2. Delete name
+3. Display names
+4. Exit
+Enter the Choice: 1
+Enter a name: Prajwal
+1. Add name
+2. Delete name
+3. Display names
+4. Exit
+Enter the Choice: 1
+Enter a name: Amrita
+1. Add name
+2. Delete name
+3. Display names
+4. Exit
+Enter the Choice: 1
+Enter a name: Kavya
+1. Add name
+2. Delete name
+3. Display names
+4. Exit
+Enter the Choice: 1
+Enter a name: Drishti
+1. Add name
+2. Delete name
+3. Display names
+4. Exit
+Enter the Choice: 3
+['Prajwal', 'Amrita', 'Kavya', 'Drishti']
+1. Add name
+2. Delete name
+3. Display names
+4. Exit
+Enter the Choice: 2
+Deleted name is: Drishti
+1. Add name
+2. Delete name
+3. Display names
+4. Exit
+Enter the Choice: 3
+['Prajwal', 'Amrita', 'Kavya']
+1. Add name
+2. Delete name
+3. Display names
+4. Exit
+Enter the Choice: 4
+Exiting program.
 CONTENT
 # ====== END PASTE ======
 
